6ebe15e03c547eb4e127c60e71151adbb6ef5c9b
surichai <surichai@future2you.com>
2019-05-23 15:36:14 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-from-array';
+  public formGroup: FormGroup;
+  public memberList: FormArray;
+
+  get memberFormGroup() {
+    return this.formGroup.get('member') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.formGroup = this.fb.group({
+      groupname: [null, Validators.compose([Validators.required])],
+      member: this.fb.array([this.createMember()])
+    });
+
+    // set contactlist to this field
+    this.memberList = this.formGroup.get('member') as FormArray;
+  }
+
+  createMember(): FormGroup {
+    return this.fb.group({
+      type: [null, Validators.compose([Validators.required])], // i.e Email, Phone
+      name: [null, Validators.compose([Validators.required])], // i.e. Home, Office
+      value: [null, Validators.compose([Validators.required, Validators.email])]
+    });
+  }
+
+  addMember() {
+    this.memberList.push(this.createMember());
+  }
+
+  removeMember(index) {
+    // this.contactList = this.form.get('contacts') as FormArray;
+    this.memberList.removeAt(index);
+  }
+
+  getmemberFormGroup(index): FormGroup {
+    // this.contactList = this.form.get('contacts') as FormArray;
+    const formGroup = this.memberList.controls[index] as FormGroup;
+    return formGroup;
+  }
+
+  submit() {
+    console.log(this.formGroup.value);
+  }
 }
